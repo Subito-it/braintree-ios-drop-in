dafd350a015d64fcdd8598671f734a035cd6df36
m.crespi <matteo.crespi@schibsted.com>
2019-07-10 15:10:33 +0200

    Add SBTDropInViewController

---
+++ BraintreeDropIn/Public/SBTDropInViewController.h
@@ -0,0 +1,11 @@
+// Copyright © 2019 Subito.it. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SBTDropInViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BraintreeDropIn/SBTDropInViewController.m
@@ -0,0 +1,11 @@
+// Copyright © 2019 Subito.it. All rights reserved.
+
+#import "SBTDropInViewController.h"
+
+@interface SBTDropInViewController ()
+
+@end
+
+@implementation SBTDropInViewController
+
+@end
